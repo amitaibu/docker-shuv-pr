47e28e210cd63253b880a9964e95a55ed5ea20e9
Amitaibu <amitai@gizra.com>
2015-03-30 16:12:44 +0300

    More work

--- main.sh
+++ main.sh
@@ -5,15 +5,15 @@ SCREENSHOT_IDS=$2
 NEW_BRANCH=$3
 ACCESS_TOKEN=$4
 
-BUILD_INFO=$(node build_info.js $BUILD_ID $ACCESS_TOKEN)
+BUILD_INFO=$(node /home/build_info.js $BUILD_ID $ACCESS_TOKEN)
 
-OWNER=$(echo BUILD_INFO | jq '.owner')
-REPO=$(echo BUILD_INFO | jq '.repo')
-BRANCH=$(echo BUILD_INFO | jq '.branch')
+OWNER=$(echo $BUILD_INFO | jq '.owner')
+REPO=$(echo $BUILD_INFO | jq '.repo')
+BRANCH=$(echo $BUILD_INFO | jq '.branch')
 
 # Create an SSH key.
 touch ~/.ssh/id_rsa
-node get_ssh.js > ~/.ssh/id_rsa
+node /home/get_ssh.js $BUILD_ID $ACCESS_TOKEN > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
 # Clone repo
@@ -22,14 +22,14 @@ mkdir clone
 
 git config --global user.email "robot@example.com"
 git config --global user.name "Robot"
-node get_hub.js > ~/.config/hub
+node /home/get_hub.js $ACCESS_TOKEN > ~/.config/hub
 
 cd clone
 hub clone -p --branch=$BRANCH --depth=1 --quiet $OWNER/$REPO .
 git checkout -b $NEW_BRANCH
 
 # Download images
-node ../download_images.js $ACCESS_TOKEN $SCREENSHOT_IDS
+node /home/download_images.js $SCREENSHOT_IDS $ACCESS_TOKEN
 
 # Push new branch
 git add --all
